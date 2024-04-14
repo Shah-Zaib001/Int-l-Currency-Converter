b10bd24c84eaa680035bba36c0ec30b480cd724b
Shahzaib <shahzjee@gmail.com>
2024-04-15 00:43:56 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blue.bold("\n Welcome to Currency Convertor\n"));
+//Define the list of currencies and their exchange rates
+let exchangeRate = {
+    "USD": 1,
+    "EUR": 0.94,
+    "JYP": 153.08,
+    "CAD": 1.38,
+    "AUD": 1.55,
+    "PKR": 277.30, //Pakistani Rupees
+};
+let userAns = await inquirer.prompt([
+    {
+        name: "fromCurrency",
+        type: "list",
+        message: "select the currency to convert from:",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "toCurrency",
+        type: "list",
+        message: "select the currency to convert into:",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: "enter the amount to convert:"
+    }
+]);
+let fromAmount = exchangeRate[userAns.fromCurrency];
+let toAmount = exchangeRate[userAns.toCurrency];
+let amount = userAns.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(`converted amount = ${chalk.green.bold(convertedAmount.toFixed(2))}`);
